18c661e8440527c72e642d230fb9c71f9f85448a
Érico Monteiro <erico.cintra@gmail.com>
2022-01-25 19:26:12 -0300

    create project

---
+++ src/main/kotlin/ericomonteiro/com/github/sales/SalesApplication.kt
@@ -0,0 +1,11 @@
+package ericomonteiro.com.github.sales
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SalesApplication>(*args)
+}
---
+++ src/test/kotlin/ericomonteiro/com/github/sales/SalesApplicationTests.kt
@@ -0,0 +1,13 @@
+package ericomonteiro.com.github.sales
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
