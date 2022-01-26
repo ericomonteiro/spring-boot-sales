9f3e3c979eb284d2f15e9ceacea52de71108c7fe
Érico Monteiro <erico.cintra@gmail.com>
2022-01-26 03:29:27 -0300

    create a tag at span for authenticated user

--- src/main/kotlin/ericomonteiro/com/github/sales/controller/HomeController.kt
+++ src/main/kotlin/ericomonteiro/com/github/sales/controller/HomeController.kt
@@ -3,7 +3,9 @@ package ericomonteiro.com.github.sales.controller
 import ericomonteiro.com.github.sales.httpclients.CatalogClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,8 +18,9 @@ class HomeController(
     private val logger: Logger = LoggerFactory.getLogger(HomeController::class.java)
 
     @GetMapping
-    fun hello(): String {
-        return "Sales System was called and after - ${catalogClient.home()}"
+    fun hello(@RequestHeader(value = HttpHeaders.AUTHORIZATION) authorization: String): String {
+        logger.info("Home controller sales service")
+        return "Sales System was called and after - ${catalogClient.home(authorization)}"
     }
 
 }
--- src/main/kotlin/ericomonteiro/com/github/sales/httpclients/CatalogClient.kt
+++ src/main/kotlin/ericomonteiro/com/github/sales/httpclients/CatalogClient.kt
@@ -1,12 +1,14 @@
 package ericomonteiro.com.github.sales.httpclients
 
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(value = "catalogClient", url = "\${catalog.httpclient.host}")
 interface CatalogClient {
 
     @GetMapping("/home")
-    fun home(): String
+    fun home(@RequestHeader(value = HttpHeaders.AUTHORIZATION) authorization: String): String
 
 }
