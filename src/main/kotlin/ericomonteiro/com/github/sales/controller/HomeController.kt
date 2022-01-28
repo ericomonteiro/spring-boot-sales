993c130cb3efa3f18e99182e940447b5c69877a8
Érico Monteiro <erico.cintra@gmail.com>
2022-01-28 00:04:53 -0300

    review with article

--- src/main/kotlin/ericomonteiro/com/github/sales/controller/HomeController.kt
+++ src/main/kotlin/ericomonteiro/com/github/sales/controller/HomeController.kt
@@ -3,9 +3,7 @@ package ericomonteiro.com.github.sales.controller
 import ericomonteiro.com.github.sales.httpclients.CatalogClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,10 +15,19 @@ class HomeController(
 
     private val logger: Logger = LoggerFactory.getLogger(HomeController::class.java)
 
+    val sampleJwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
     @GetMapping
-    fun hello(@RequestHeader(value = HttpHeaders.AUTHORIZATION) authorization: String): String {
+    fun hello(): String {
         logger.info("Home controller sales service")
-        return "Sales System was called and after - ${catalogClient.home(authorization)}"
+        return "Sales System was called and after - ${catalogClient.home(sampleJwtToken, generateAnyBody())}"
+    }
+
+    private fun generateAnyBody(): MutableMap<String, String> {
+        val body = mutableMapOf<String, String>()
+        body["field-01"] = "some value"
+        body["field-02"] = "another value"
+        return body
     }
 
 }
--- src/main/kotlin/ericomonteiro/com/github/sales/httpclients/CatalogClient.kt
+++ src/main/kotlin/ericomonteiro/com/github/sales/httpclients/CatalogClient.kt
@@ -2,13 +2,17 @@ package ericomonteiro.com.github.sales.httpclients
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.HttpHeaders
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(value = "catalogClient", url = "\${catalog.httpclient.host}")
 interface CatalogClient {
 
-    @GetMapping("/home")
-    fun home(@RequestHeader(value = HttpHeaders.AUTHORIZATION) authorization: String): String
+    @PostMapping("/home")
+    fun home(
+        @RequestHeader(value = HttpHeaders.AUTHORIZATION) authorization: String,
+        @RequestBody anyBody: Map<String, Any>
+    ): String
 
 }
